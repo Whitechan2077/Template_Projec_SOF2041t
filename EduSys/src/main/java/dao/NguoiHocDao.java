262212755ed1f30a0b418918559e02cf3bd80c84
Hoàng Dũng <dungbui8198@gmail.com>
2023-09-26 13:59:17 +0700

    Update NguoiHocDao.java

--- EduSys/src/main/java/dao/NguoiHocDao.java
+++ EduSys/src/main/java/dao/NguoiHocDao.java
@@ -18,49 +18,63 @@ import org.hibernate.query.Query;
  * @author buidu
  */
 public class NguoiHocDao implements DAO<NguoiHoc, String>{
-    private Session session;
     private SessionFactory factory;
-    private Transaction transaction;
 
     
     public NguoiHocDao(SessionFactory factory) {
         this.factory = factory;
-        this.session = factory.openSession();
-        this.transaction = session.getTransaction();
     }
 
     @Override
     public void insert(NguoiHoc e) {
-        transaction.begin();
-        session.persist(e);
-        transaction.commit();
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.getTransaction();
+            transaction.begin();
+            session.persist(e);
+            transaction.commit();
+        }
     }
 
     @Override
     public void delete(NguoiHoc e) {
-       transaction.begin();
-       session.remove(e);
-       transaction.commit();    }
+   Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.getTransaction();
+            transaction.begin();
+            session.remove(e);
+            transaction.commit();
+        }   }
 
     @Override
     public void update(NguoiHoc e) {
-       transaction.begin();
-       session.merge(e);
-       transaction.commit();    }
+ Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.getTransaction();
+            transaction.begin();
+            session.merge(e);
+            transaction.commit();
+        };    }
 
     @Override
     public List<NguoiHoc> getAll() {
-         Query<NguoiHoc> q = session.getNamedQuery("NguoiHoc.findAll");
-         return  q.getResultList();
+        try (Session session = factory.openSession()) {
+            Query<NguoiHoc> q = session.getNamedQuery("NguoiHoc.findAll");
+            return  q.getResultList();
+        }
     }
 
     @Override
     public NguoiHoc getByID(String id) {
-        Query<NguoiHoc> q = session.getNamedQuery("NguoiHoc.findByMaNH");
-        q.setParameter("maNH", id);
-        return q.getSingleResult();
+        try (Session session = factory.openSession()) {
+            Query<NguoiHoc> q = session.getNamedQuery("NguoiHoc.findByMaNH");
+            q.setParameter("maNH", id);
+            return q.getSingleResult();
+        }
     }
     public List<NguoiHoc>getNguoiHoc(){
-        return session.createQuery("SELECT nh FROM NguoiHoc nh WHERE nh NOT IN (SELECT hv.maNH FROM HocVien hv)").getResultList();
+        try (Session session = factory.openSession()) {
+            return session.createQuery("SELECT nh FROM NguoiHoc nh WHERE nh NOT IN (SELECT hv.maNH FROM HocVien hv)").getResultList();
+        }
     }
 }
